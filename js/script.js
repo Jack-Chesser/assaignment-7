9d3e36724681e7cb93dc087555b458cf28377e36
Jack-Chesser <chessjac000@gmail.com>
2026-03-22 19:45:45 -0500

    finishes the styling of the buttons

--- js/script.js
+++ js/script.js
@@ -46,3 +46,70 @@ answerButtons.forEach(button => {
         console.log("Current Answers:", userAnswers);
     });
 });
+
+// Submit button
+const submitBtn = document.getElementById("submit-btn");
+const resultDiv = document.getElementById("result");
+
+submitBtn.addEventListener("click", () => {
+
+    // Make sure all questions are answered
+    if (!userAnswers.universe || !userAnswers.weapon || !userAnswers.heroType) {
+        resultDiv.innerHTML = "<p class='text-danger'>Please answer all questions!</p>";
+        return;
+    }
+
+    let finalHero = "";
+
+    // MARVEL PATH
+    if (userAnswers.universe === "marvel") {
+
+        if (userAnswers.weapon === "money-tech") {
+            finalHero = "Iron Man";
+        }
+        else if (userAnswers.weapon === "strength-speed") {
+            finalHero = "Hulk";
+        }
+        else if (userAnswers.weapon === "projectiles") {
+            finalHero = "Spider-Man";
+        }
+        else if (userAnswers.weapon === "close-combat") {
+            finalHero = "Black Panther";
+        }
+
+        // Personality overrides
+        if (userAnswers.heroType === "leader") finalHero = "Captain America";
+        if (userAnswers.heroType === "team-player") finalHero = "War Machine";
+        if (userAnswers.heroType === "good-heart") finalHero = "Spider-Man";
+        if (userAnswers.heroType === "anti-hero") finalHero = "Hulk";
+    }
+
+    // DC PATH
+    else if (userAnswers.universe === "dc") {
+
+        if (userAnswers.weapon === "money-tech") {
+            finalHero = "Batman";
+        }
+        else if (userAnswers.weapon === "strength-speed") {
+            finalHero = "Superman";
+        }
+        else if (userAnswers.weapon === "projectiles") {
+            finalHero = "Green Arrow";
+        }
+        else if (userAnswers.weapon === "close-combat") {
+            finalHero = "Wonder Woman";
+        }
+
+        // Personality overrides
+        if (userAnswers.heroType === "leader") finalHero = "Wonder Woman";
+        if (userAnswers.heroType === "team-player") finalHero = "Cyborg";
+        if (userAnswers.heroType === "good-heart") finalHero = "Superman";
+        if (userAnswers.heroType === "anti-hero") finalHero = "Batman";
+    }
+
+    // Display result
+    resultDiv.innerHTML = `
+        <h2>Your Superhero Is:</h2>
+        <h1>${finalHero}!</h1>
+    `;
+});
